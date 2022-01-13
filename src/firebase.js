8a5c7f8f61b564cacafea51b8053ce0aad91b658
Mikel Ferrer <ferremikel@gmail.com>
2022-01-13 10:56:23 +0100

    13ENE2022

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA0_FkgvlJjaNYsG6uSE-Td626xQeVa_08",
-    authDomain: "agenda-iw-48a01.firebaseapp.com",
-    projectId: "agenda-iw-48a01",
-    storageBucket: "agenda-iw-48a01.appspot.com",
-    messagingSenderId: "167008297125",
-    appId: "1:167008297125:web:16e9bc5bd8ec7f7dad2d4c"
-  };
+  apiKey: "AIzaSyA0_FkgvlJjaNYsG6uSE-Td626xQeVa_08",
+  authDomain: "agenda-iw-48a01.firebaseapp.com",
+  projectId: "agenda-iw-48a01",
+  storageBucket: "agenda-iw-48a01.appspot.com",
+  messagingSenderId: "167008297125",
+  appId: "1:167008297125:web:2a806414b1b976d2ad2d4c"
+};
 
 const app = initializeApp(firebaseConfig);
 
